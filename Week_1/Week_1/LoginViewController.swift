8c15ba1286a81aa667a84c59e6d75ca5d655496f
정정욱 <jeonguk29@naver.com>
2025-04-05 16:12:41 +0900

    [Week1] : 화면 전환 익히기

---
+++ Week_1/Week_1/LoginViewController.swift
@@ -0,0 +1,90 @@
+//
+//  LoginViewController.swift
+//  Week_1
+//
+//  Created by 정정욱 on 4/5/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 69, y: 161, width: 236, height: 44))
+        label.text = "동네라서 가능한 모든것\n당근에서 가까운 이웃과 함께해요."
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.font = .boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let idTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
+        textField.placeholder = "아이디"
+        textField.font = .systemFont(ofSize: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
+        textField.placeholder = "비밀번호"
+        textField.font = .systemFont(ofSize: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        return textField
+    }()
+    
+    var loginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 21, y: 422, width: 332, height: 58))
+        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.setTitle("로그인하기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        button.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        addViews()
+    }
+    
+    private func addViews() {
+        view.addSubview(titleLabel)
+        view.addSubview(idTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+    }
+    
+    //    private func setLayout() {
+    //        [titleLabel, idTextField, passwordTextField, loginButton].forEach {
+    //            self.view.addSubview($0)
+    //        }
+    //    }
+    
+    
+    //MARK: Action
+    @objc
+    private func loginButtonDidTap() {
+        presentToWelcomeVC()
+        //pushToWelcomeVC()
+    }
+    
+    private func presentToWelcomeVC() {
+        let welcomeViewController = WelcomeViewController()
+        welcomeViewController.modalPresentationStyle = .fullScreen
+        self.present(welcomeViewController, animated: true)
+    }
+    
+    private func pushToWelcomeVC() {
+        let welcomeViewController = WelcomeViewController()
+        self.navigationController?.pushViewController(welcomeViewController, animated: true)
+    }
+    
+}
+
+#Preview{
+    LoginViewController()
+}
---
+++ Week_1/Week_1/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  Week_1
+//
+//  Created by 정정욱 on 4/5/25.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
